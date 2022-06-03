03c0bce298d78b3cc6eea113f8d1a56504622b2b
Kirill Osenkov <github@osenkov.com>
2022-06-02 19:50:01 -0700

    Generate XAML

--- src/ColorTools/Program.cs
+++ src/ColorTools/Program.cs
@@ -27,34 +27,59 @@ class Program
             .OrderBy(p => p.Name)
             .ToArray();
 
-        var colorProperties = typeof(Colors)
+        var colorProperties = typeof(SystemColors)
             .GetProperties()
             .Where(p => p.PropertyType == typeof(Color))
             .OrderBy(p => p.Name)
             .ToArray();
 
+        sb.AppendLine();
+
         foreach (var prop in brushProperties)
         {
             var brush = prop.GetValue(null) as SolidColorBrush;
+
+            var name = prop.Name;
+            var colorText = brush.Color.ToString().Replace("#FF", "#");
             var rect = new Rectangle() { Width = 100, Height = 20, Fill = brush };
             var panel = new StackPanel() { Orientation = Orientation.Horizontal };
-            panel.Children.Add(new TextBlock() { Text = prop.Name, Width = 200 });
+            panel.Children.Add(new TextBlock() { Text = name, Width = 200 });
             panel.Children.Add(rect);
-            panel.Children.Add(new TextBlock() { Text = brush.Color.ToString(), Margin = new Thickness(8, 0, 8, 0) });
+            panel.Children.Add(new TextBlock() { Text = colorText, Margin = new Thickness(8, 0, 8, 0) });
             content.Items.Add(panel);
+
+            sb.AppendLine($"  <SolidColorBrush x:Key=\"{{x:Static SystemColors.{name}Key}}\" Color=\"{colorText}\" />");
         }
+
+        sb.AppendLine();
 
         foreach (var prop in colorProperties)
         {
             var color = (Color)prop.GetValue(null);
-            sb.AppendLine($"{color.R / 255.0:X2}, {color.G / 255.0:X2}, {color.B / 255.0:X2},");
+            var name = prop.Name;
+            //sb.AppendLine($"{color.R / 255.0:X2}, {color.G / 255.0:X2}, {color.B / 255.0:X2},");
             //sb.AppendLine($"{color.ScR}, {color.ScG}, {color.ScB},");
+
+            sb.AppendLine($"  <Color x:Key=\"{{x:Static SystemColors.{name}Key}}\" R=\"{color.R}\" G=\"{color.G}\" B=\"{color.B}\" A=\"{color.A}\"/>");
         }
 
-        Clipboard.SetText(sb.ToString());
+        var text = sb.ToString();
+
+        text = string.Format(GetTemplate(), text);
+
+        Clipboard.SetText(text);
 
         window.Content = content;
 
         app.Run(window);
+    }
+
+    public static string GetTemplate()
+    {
+        return @"<ResourceDictionary
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"">
+{0}
+</ResourceDictionary>";
     }
 }
